1e4c21cab5f22475bdf3fa42c67bdf0ec1c74410
Rahul Nolakha <djangopythonlearner@gmail.com>
2019-07-02 23:14:14 +0530

    ad validation on registration form

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,4 @@
+import simplejson as simplejson
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, render_to_response
 from django.views import View
@@ -23,27 +24,23 @@ class SignUp(CreateView):
         emailv = request.POST.get('email',None)
         phonenov = request.POST.get('phoneno',None)
         passwordv = request.POST.get('password',None)
-        cpasswordv = request.POST.get('cpassword',None)
-        if(len(fnamev)<3):
-            fnameError = 'Name must contain min 3 characters.'
-        myDict = {'fnameError':fnameError}
-        print(fnamev)
-        print(lnamev)
-        print(emailv)
-        print(phonenov)
-        print(passwordv)
-        print(cpasswordv)
-        return render(request,'sign_up.html',)
-        # print(request.POST['fname'])
+        try:
+            isPersonExist = Person.objects.get(email=emailv)
+        except Person.DoesNotExist:
+            isPersonExist = False
 
+        if isPersonExist:
+            data = simplejson.dumps({'success': False, 'error': True, 'errorMsg': "Email already exist"})
+            return HttpResponse(data, content_type='application/json')
 
-        # person = Person(
-        #     f_name=request.POST['fname'],
-        #     l_name=request.POST['lname'],
-        #     email=request.POST['email'],
-        #     mobile=request.POST['phoneno'],
-        #     # password=request.POST['password']
-        # )
-        # person.save()
-        return HttpResponse({'user': 'Rahul Nolakha'})
+        person = Person(
+            f_name=request.POST['fname'],
+            l_name=request.POST['lname'],
+            email=request.POST['email'],
+            mobile=request.POST['phoneno'],
+            # password=request.POST['password']
+        )
+        person.save()
+        data = simplejson.dumps({'success': True})
+        return HttpResponse(data, content_type='application/json')
 
--- static/js/registration.js
+++ static/js/registration.js
@@ -1,48 +1,16 @@
-/*
-$('#signup').click(function (e){
-    e.preventDefault();
-   var fname = $('[name=fname]').val()
-    var lname = $('[name=lname]').val()
-    var email = $('[name=email]').val()
-    var phoneno = $('[name=phoneno]').val()
-    var password = $('[name=password]').val()
-    var cpassword = $('[name=cpassword]').val()
-
-    if(fname.length<3){
-        $('.errors').html('Name min 3 char.')
-
-    }
-    // console.log(fname)
-    // console.log(lname)
-    // console.log(email)
-    // console.log(phoneno)
-    // console.log(password)
-    // console.log(cpassword)
-});
-
-*/
-
-
-
-
-// $('#signup').click(function () {
-//     var fname = $('[name=fname]').val()
-//     var lname = $('[name=lname]').val()
-//     console.log(fname)
-//     console.log(lname)
-//
-// });
-
-
-
-
-
 $('#signup').click(function(e) {
     e.preventDefault();
    if (isSignUpFormValid() == true) {
         $.post('http://localhost:8000/signup/', $('#signup_form').serialize())
-            .done(function(data, status) {
-                alert(data.user, status);
+            .done(function(data) {
+                if (data.success) {
+                    $('#formSuccess').html('Record created successfully').fadeIn().delay(5000).fadeOut();
+                    $('#formError').html('');
+                    $('#signup_form')[0].reset();
+                } else {
+                    $('#formError').html(data.errorMsg).fadeIn().delay(5000).fadeOut();
+                    $('#formSuccess').html('');
+                }
             }).fail(function(error) {
                 alert('error' +error);
         });
@@ -50,21 +18,83 @@ $('#signup').click(function(e) {
 })
 
 function isSignUpFormValid() {
-     var first_name = $('[name=fname]');
-    var last_name = $('[name=lname]').val();
-    var email = $('[name=email]').val();
-    var password = $('[name=password]').val();
-    var cpassword = $('[name=cpassword]');
-    var phoneno = $('[name=phoneno]').val();
+    var first_name = $('[name=fname]');
+    var last_name = $('[name=lname]');
+    var email = $('[name=email]');
+    var phoneno = $('[name=phoneno]');
     var isValid = true;
-    if(!first_name.val()) {
-        first_name.parent().find('.errors').html('Please enter username');
+    if(first_name.val().length < 3) {
+        first_name.parent().find('.errors').html('Name must contain min 3 characters..');
+        isValid = false;
+    } else {
+        var fn = /^[a-z]+$/i.test(first_name.val());
+        first_name.parent().find('.errors').html();
+        if(fn == false){
+            first_name.parent().find('.errors').html('Name contains only alphabets..!')
+            isValid = false
+        }
+    }
+    // validate last name data
+    if(last_name.val().length < 3) {
+        last_name.parent().find('.errors').html('Last Name must contain min 3 characters..');
+        isValid = false;
+    } else {
+        last_name.parent().find('.errors').html('');
+        var ln = /^[a-z]+$/i.test(last_name.val());
+        if(ln == false){
+            last_name.parent().find('.errors').html('Last Name contains only alphabets..!')
+            isValid = false
+        }
+    }
+    // validate email address
+    var emailval = $('[name=email]').val();
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    eres = re.test(String(emailval).toLowerCase());
+    if(eres){
+        email.parent().find('.errors').addClass('hide');
+    } else {
+        email.parent().find('.errors').removeClass('hide');
         isValid = false;
     }
-    if(1) {
-        cpassword.parent().find('.errors').html('Please enter password');
+
+    var phone = /^\d{10}$/;
+    if(!phoneno.val().match(phone)) {
+         phoneno.parent().find('.errors').html('Mobile no contain 10 digits..!');
+         isValid = false;
+    } else {
+        phoneno.parent().find('.errors').html('');
+    }
+    var password = $('[name=password]');
+    var cpassword = $('[name=cpassword]');
+    var pvalue = password.val();
+    var cpvalue = cpassword.val();
+    if(pvalue == cpvalue){
+        cpassword.parent().find('.errors1').html('');
+    } else {
+        cpassword.parent().find('.errors1').html('Password mismatch..');
         isValid = false;
     }
     return isValid;
 
 }
+
+$('[name=phoneno]').on('change', function() {
+    var phoneno = $('[name=phoneno]');
+    var phone = /^\d{10}$/;
+    if(!phoneno.val().match(phone)) {
+         phoneno.parent().find('.errors').html('Mobile no contain 10 digits..!');
+         isValid = false;
+    } else {
+        phoneno.parent().find('.errors').html('');
+    }
+});
+
+
+$('[name=fname]').on('input', function() {
+    var first_name = $('[name=fname]');
+    var fn = /^[a-z]+$/i.test(first_name.val());
+    first_name.parent().find('.errors').html('');
+    if(fn == false){
+        first_name.parent().find('.errors').html('Name contains only alphabets..!')
+    }
+});
